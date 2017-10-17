0386c7a6e858f330c1f4dc1942d8b1025c64ac4b
Felipe <Felipe@192.168.1.129>
2017-10-17 11:14:05 +0200

    Definición atributo puntosDevida

--- rolegame/src/rolegame/Troll.java
+++ rolegame/src/rolegame/Troll.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rolegame;
 
-/**
- *
- * @author manolo
- */
 public class Troll {
     private String nombre;
+    private int puntosDevida;
     
 }
